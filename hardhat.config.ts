97bde98db8b6d19bdb5a0ca811505114b334cf99
dhruv patel <d7511162@gmail.com>
2026-01-26 09:39:05 +0530

    some changes
    changes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,9 @@ import '@openzeppelin/hardhat-upgrades';
 import 'solidity-coverage';
 import '@nomiclabs/hardhat-solhint';
 import '@primitivefi/hardhat-dodoc';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -14,6 +17,20 @@ const config: HardhatUserConfig = {
         enabled: true,
         runs: 200,
       },
+    },
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || '',
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+      gasPrice: 10_000_000_000 // 10 gwei
+
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY || '',
     },
   },
   gasReporter: {
